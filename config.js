993f32b13e84a88b069813192a2f83878cfaffad
richboy666 <berich00012@gmail.com>
2025-05-26 11:37:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~SVI0GRqJ#II5cDXc2QIR8ivrwzGMNI_DMOcPgOvU4SDEl6jh-5rY",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~ONY1hZDS#Iin3pIjXYDNO3Y-rLRum0p9BmlQlQBVgWEjU5mGQLWU",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "private",
@@ -45,18 +45,18 @@ module.exports = {
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "false",
     PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    FAKE_TYPING: process.env.FAKE_TYPING || "true",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "251 96 642 2462",
-    OWNER_NAME: process.env.OWNER_NAME || "𝖦 𝖮 𝖲 𝖳 𝖠 𝖵 𝖮 :(🕴",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "+251 97 678 6441",
+    OWNER_NAME: process.env.OWNER_NAME || "qooqjabiye",
     READ_CMD: process.env.READ_CMD || "false",
     BOT_NAME: process.env.BOT_NAME || "➺DARK-SILENCE-MD",
     STATUS_REPLY: process.env.STATUS_REPLY || "",
-    STATUS_REACT: process.env.STATUS_REACT || "true",
+    STATUS_REACT: process.env.STATUS_REACT || "false",
     ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'log' if you want to resend deleted message in ib chat 
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39"// omdbapi.com
